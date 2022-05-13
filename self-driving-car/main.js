37d94d1c8b4281100f81fe600ccfede41e8c76dd
Paul S Flanagan <paulsflanagan@gmail.com>
2022-05-14 00:19:05 +0100

    Update main.js

--- self-driving-car/main.js
+++ self-driving-car/main.js
@@ -15,7 +15,7 @@ function animate(){
   canvas.height = window.innerHeight;
   
   ctx.save();
-  ctx.translate(0,-car.y+canvas.height*0.5);
+  ctx.translate(0,-car.y+canvas.height*0.7);
   road.draw(ctx);
   car.draw(ctx);
   
